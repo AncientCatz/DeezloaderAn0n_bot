4d3652759875ca85f6bd508647a442e7b0a0fdf5
* <admin@ancientcatz.eu.org>
2020-08-12 22:21:57 +0700

    update

---
+++ __main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except Exception:
+    pass
+# end try
+
+import dwsongs-normal as start
+start()
